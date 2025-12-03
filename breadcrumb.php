b16ffb2d611454d06a360c44cee5850f6484dfb2
Mehmet ARIK <othemeswp@gmail.com>
2025-12-03 15:34:51 +0300

    Add custom breadcrumb function

---
+++ breadcrumb.php
@@ -0,0 +1,23 @@
+function custom_breadcrumbs() {
+    $separator = ' > ';
+    $home = 'Anasayfa';
+
+    echo '<div class="breadcrumbs">';
+
+    if (!is_front_page()) {
+        echo '<a href="' . home_url() . '">' . $home . '</a>' . $separator;
+    }
+
+    if (is_category() || is_single()) {
+        the_category(' • ');
+        if (is_single()) {
+            echo $separator . get_the_title();
+        }
+    } elseif (is_page()) {
+        echo get_the_title();
+    } elseif (is_search()) {
+        echo 'Arama sonuçları: ' . get_search_query();
+    }
+
+    echo '</div>';
+}
